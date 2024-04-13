cd4729de9bafee29747a00968c539ff870be354d
AmauriBlanco <blancoamauri@gmail.com>
2024-04-13 01:11:55 -0300

    up

---
+++ src/data/data.ts
@@ -0,0 +1,17 @@
+export const categories = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C++",
+    "Ruby",
+    "Swift",
+    "HTML/CSS",
+    "PHP",
+    "C#",
+    "TypeScript",
+    "Go",
+    "Kotlin",
+    "Rust",
+    "SQL",
+    "Shell",
+];
